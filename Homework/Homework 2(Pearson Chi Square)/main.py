08ba0ebc8f13c4a10392913b227f26b21374edea
BigWillieN <69094722+BigWillieN@users.noreply.github.com>
2020-11-29 21:03:53 +0300

    Add files via upload

---
+++ Homework/Homework 2(Pearson Chi Square)/main.py
@@ -0,0 +1,25 @@
+
+import random
+def fair_dice():
+#This function is written to see if the hypothetical die is fair or not.
+     roll = int(input("Enter how many times you want to roll your die: "))
+     count= 0
+     results = []
+     while (count< roll):
+         dice_value = random.randint(1,6)
+         results.append(dice_value)
+         count = count+1
+     x = results.count(1)
+     y = results.count(2)
+     z = results.count(3)
+     d = results.count(4)
+     e = results.count(5)
+     f = results.count(6)
+     chisquared = ((x-(roll/6))**2)/(roll/6) + ((y-(roll/6))**2)/(roll/6) + ((z-(roll/6))**2)/(roll/6) + ((d-(roll/6))**2)/(roll/6) + ((e-(roll/6))**2)/(roll/6) + ((f-(roll/6))**2)/(roll/6)
+     if chisquared <= 5:
+        print("Dice is fair.")
+     else:
+        print("Dice isn't fair.")
+     print(chisquared)
+     print(x, y, z, d, e, f)
+fair_dice()
